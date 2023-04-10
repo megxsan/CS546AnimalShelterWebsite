0e80f80dcdec48641102d620c4b8853f8be4a132
tylerklane <105730362+tylerklane@users.noreply.github.com>
2023-04-10 15:37:40 -0400

    Add files via upload

--- data/users.js
+++ data/users.js
@@ -39,6 +39,41 @@ const exportedMethods = {
         if (myUser === null) throw 'No user with that ID';
         myUser._id = myUser._id.toString();
         return myUser;
+    },
+
+    async update(id, firstName, lastName, age, email, password){
+        id = validation.checkId(id, "User ID");
+        firstName = validation.checkString(firstName, "First Name");
+        lastName = validation.checkString(lastName, "Last Name");
+        age = validation.checkAge(age);
+        email = validation.checkEmail(email);
+        password = validation.checkString(password, "Password");
+        const userCollection = await users();
+        const myUser = await userCollection.findOne({_id: new ObjectId(id)});
+        if (myUser === null) throw 'No user with that ID';
+        const updatedUser = {
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+            email: email,
+            password: password,
+            dogs: myUser.dogs,
+            quizResult: myUser.quizResult,
+            application: myUser.application,
+            accepted: myUser.accepted,
+            pending: myUser.pending,
+            rejected: myUser.rejected,
+            liked: myUser.liked,
+            disliked: myUser.disliked
+        };
+        const updatedInfo = await userCollection.findOneAndReplace(
+            {_id: new ObjectId(id)},
+            updatedUser,
+            {returnDocument: 'after'}
+        );
+        if (updatedInfo.lastErrorObject.n === 0) throw [404, `Error: Update failed! Could not update post with id ${id}`];
+        updatedInfo.value._id = updatedInfo.value._id.toString();
+        return updatedInfo.value;
     }
 };
 
