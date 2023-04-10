2e87dc7b4cf30fd271a0a826bbff6574dd211142
megxsan <msanford@stevens.edu>
2023-04-10 14:23:24 -0400

    organizing files into folders

---
+++ data/users.js
@@ -0,0 +1,36 @@
+import { users } from "../config/mongoCollections.js";
+import { ObjectId } from "mongodb";
+import validation from "../usersHelpers.js";
+
+const exportedMethods = {
+    async create(firstName, lastName, age, email, password){
+        firstName = validation.checkString(firstName, "First Name");
+        lastName = validation.checkString(lastName, "Last Name");
+        age = validation.checkAge(age);
+        email = validation.checkEmail(email);
+        password = validation.checkString(password, "Password");
+        let newUser = {
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+            email: email,
+            password: password,
+            dogs: [],
+            quizResult: [],
+            application: {},
+            accepted: [],
+            pending: [],
+            rejected: [],
+            liked: [],
+            disliked: []
+        };
+        const userCollection = await users();
+        const insertInfo = await userCollection.insertOne(newUser);
+        if (!insertInfo.acknowledged || !insertInfo.insertedId){
+            throw 'Could not add user';
+        }
+        return newUser;
+    }
+};
+
+export default exportedMethods;
