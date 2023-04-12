7d1f9632577d6f2fba3b2c7cecac58d9e95addf3
megxsan <msanford@stevens.edu>
2023-04-11 22:12:54 -0400

    updated the route/index.js file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-//import bandRoutes from './bands.js';
-//import albumRoutes from './albums.js';
+import dogRoutes from './dogs.js';
+import userRoutes from './user.js';
+import appRoutes from './application.js';
 
 const constructorMethod = (app) => {
-    // app.use('/bands', bandRoutes);
-    // app.use('/albums', albumRoutes)
+    //I'm not sure if this is what we want or not  
+    app.use('/', userRoutes);
+    app.use('/dog', dogRoutes)
+    app.use('/app', appRoutes)
+
 
     app.use('*', (req, res) => {
         res.status(404).json({error: 'Route Not Found'});
