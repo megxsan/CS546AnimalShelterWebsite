83dda6a9817cc83a814d1fac9624ed8e8d91f36c
tylerklane <105730362+tylerklane@users.noreply.github.com>
2023-04-15 12:29:21 -0400

    Add files via upload

--- validation.js
+++ validation.js
@@ -1,5 +1,35 @@
 import {ObjectId} from 'mongodb';
 
+export function inputChecker(input){
+  if(!input) throw `Input is required`;
+}
+export function boolChecker(input){
+  if(typeof input != 'boolean') throw `A boolean is required`;
+}
+export function numChecker(input){
+  if(typeof input != 'number') throw `A number is required`;
+  if(isNaN(input) === true) throw `A number is required`;
+  if(input != Math.floor(input)) throw `Decimals are not valid`;
+}
+export function stringChecker(input){
+  if(typeof input != 'string') throw `Input must be of type string`;
+  if(input.trim().length === 0) throw `String cannot be empty`;
+  input = input.trim();
+  return input;
+}
+
+export function nameChecker(input){
+  //allowed ascii codes: 32, 45, 65-90, 97-122
+  for(let i = 0; i < input.length; i++){
+      if((input.charCodeAt(input[i]) != 32 || input.charCodeAt(input[i]) != 45) && !(input.charCodeAt(input[i]) >= 65 && (input.charCodeAt(input[i]) <= 90)) && !(input.charCodeAt(input[i]) >= 97 && (input.charCodeAt(input[i]) <= 122))){
+          throw `Invalid character present in name`;
+      }
+  }
+}
+export function arrayChecker(input){
+  if(!Array.isArray(input)) throw `An array is required`;
+}
+
 const exportedMethods = {
   checkId(id, varName) {
     if (!id) throw `Error: You must provide a ${varName}`;
@@ -69,6 +99,32 @@ const exportedMethods = {
     weight = weight.toFixed(1);
     return weight;
   },
+
+  checkArrEqual(arr1, arr2){
+    if(arr1.length !== arr2.length) return false;
+    let sortedArr1 = arr1.sort();
+    let sortedArr2 = arr2.sort();
+    for (let i = 0; i < sortedArr1.length; i++){
+      if (sortedArr1[i] !== sortedArr2[i]) return false;
+    }
+    return true;
+  },
+
+  checkAge(num){
+    if(typeof num !== 'number') throw 'Age must be a number';
+    if(!Number.isInteger(num)) throw 'Age must be an integer';
+    if(num < 18 || num > 120) throw 'You are too young or have provided a fake age';
+    return num;
+  },
+
+  checkEmail(email){
+    if(typeof email !== 'string') throw 'Email must be a string';
+    email = email.trim();
+    if(email.length === 0) throw 'Email must be more than an empty string';
+    let emailEnd = email.slice(-4);
+    if (emailEnd !== '.com' && emailEnd !== '.net' && emailEnd !== '.edu' && emailEnd !== '.gov' && emailEnd !== '.org') throw 'You must provide a valid email';
+    return email;
+  }
 };
 
 export default exportedMethods;
