234516a2aa8210d8d1aadf2fdd005511e564578f
Egnifluro <126028000+Egnifluro@users.noreply.github.com>
2023-04-15 12:00:55 -0400

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,4 +1,4 @@
-// YOU WILL NEED TO CHANGE THE DB NAME TO MATCH THE REQUIRED DB NAME IN THE ASSIGNMENT SPECS!!!
+// NOTE: YOU WILL NEED TO CHANGE THE DB NAME TO MATCH THE REQUIRED DB NAME IN THE ASSIGNMENT SPECS!!!
 export const mongoConfig = {
   // serverUrl: 'mongodb://127.0.0.1:27017',
   serverUrl: 'mongodb+srv://ilovedogs:PatrickHill@cs546cluster.7shp1ex.mongodb.net/?retryWrites=true&w=majority',
