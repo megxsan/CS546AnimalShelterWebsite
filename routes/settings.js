473b57b3e211e41b4b91d953d030d838f0b5920a
megxsan <msanford@stevens.edu>
2023-04-17 13:40:39 -0400

    started settings routes

--- routes/settings.js
+++ routes/settings.js
@@ -12,21 +12,39 @@ router
         /*  Get 
                 -Seeing status (accepted, rejected, pending) of all applications sent out
         */
-
-    });
+        if(!req.sessions.user._id){
+                res.render('error', {title: "Seetings Error", error: "Must be signed in to access statuses"});
+        }
+        res.render('status', {title: "Status", id: req.sessions.user._id});
+        });
 
 router
-	.route("/")
+	.route("/edit")
 	.get(async (req, res) => {
 	/*  Get 
 			-Seeing edit settings form
 	*/
-
+    if(!req.sessions.user._id){
+        res.render('error', {title: "Settings Error", error: "Must be signed in to change your settings"});
+    }
+        res.render('settings', {title: "Settings", id: req.sessions.user._id});
     })
     .patch(async (req, res) => {
         /*  Patch 
                 -Recieving edit settings form
         */
+        if(!req.sessions.user._id){
+            res.render('error', {title: "Settings Error", error: "Must be signed in to change your settings"});
+        }
+        let settingInputs = req.body;
+        try{
+            //error check all setting inputs
+            
+        }catch(e){
+            res.render('error', {title: "Settings Error", error: "Must be signed in to change your settings"})
+        }
+
+        //update informaiton in the database using a user update function
 
     });
 
