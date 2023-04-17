3f4a8aea80eaec32df13ced9b3ee8651335342b9
megxsan <msanford@stevens.edu>
2023-04-17 14:01:28 -0400

    added to the dogs route

--- routes/dogs.js
+++ routes/dogs.js
@@ -13,8 +13,22 @@ router
                 -Displaying all the data on the dog
                 -Seeing likes & comments
         */
+                if(!req.sessions.user._id){
+                        res.render('error', {title: "DogID Error", error: "Must be signed in to change your settings"});
+                }
+                try{
+                        req.params.dogId = validation.checkId(req.params.dogId, "Dog ID");
+                }catch(e){
+                        res.render('error', {title: "DogID Error", error: e}).status(400);
+                }
+                let dog = {};
+                try{
+                        dog = await dogData.getDogById(req.params.dogId);
+                }catch(e){
+                        res.render('error', {title: "DogID Error", error: e}).status(404);
 
-
+                }
+                res.render('dogs', {title: "My Dogs", dog: dog}).status(200);
     })
     .patch(async (req, res) => {
         /*  Patch 
@@ -22,7 +36,35 @@ router
                 -Clicking a like
                 -Adding a comment
         */
+        if(!req.sessions.user._id){
+                res.render('error', {title: "DogID Error", error: "Must be signed in to change your settings"});
+        }
+        try{
+                //checking to make sure only like happened, only comment happened, or only application happened
+        }catch(e){
+                res.render('error', {title: "Error"}).status(400);
+        }
 
+        let dog = req.params.dogId;
+        if(!req.body.likes && !req.body.comments && !req.body.interest){
+                res.render('error', {title:"Error"});
+        }
+        let foundDog = {};
+        try{
+                foundDog = await dogData.getDogById(req.params.dogId);
+        }catch(e){
+                res.render('error', {title: "DogID Error", error: e}).status(404);
+        }
+        //doing something different in each case
+        if(req.body.like){
+                //increment the likes in the dog by 1
+        }
+        if(req.body.interest){
+                //?
+        }
+        if(req.body.comments){
+                //append the comment to the array
+        }
     });
 
  export default router;
