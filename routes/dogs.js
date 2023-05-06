59360c66bba43e67283a9ee4ed930dadc0146718
megxsan <msanford@stevens.edu>
2023-05-06 17:15:55 -0400

    updated bug in dog routes

--- routes/dogs.js
+++ routes/dogs.js
@@ -35,9 +35,11 @@ router
 		try {
 			dog = await dogData.getDogById(req.params.dogId);
 			user = await userData.getUserById(dog.userId);
-			currUser = await userData.getUserById(req.session.user._id);
+			if(signedIn){
+				currUser = await userData.getUserById(req.session.user._id);
+			}
 		} catch (e) {
-			res.status(404).render("pages/homepage", { title: "DogID Error", signedIn: false});
+			return res.status(404).render("pages/homepage", { title: "DogID Error", signedIn: false});
 		}
 		res.status(200).render("pages/singledog", {
 			dog: dog,
